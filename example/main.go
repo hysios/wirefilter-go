79c136a93776f4e2e7173f22b5f79e0ec25b4793
robin <186645+c93614@users.noreply.github.com>
2021-04-20 10:00:18 +0000

    first release, but it doesn't really work

--- example/main.go
+++ example/main.go
@@ -1,18 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log"
-
-	"github.com/hysios/wirefilter"
+	"wirefilter"
 )
 
 func main() {
-	scheme := wirefilter.CreateScheme()
-	defer scheme.Close()
+	// https://developers.cloudflare.com/firewall/cf-firewall-language/operators
+	log.Print("wirefilter version: ", wirefilter.Version())
 
-	filter, err := scheme.ParerFilter(`http.method eq "POST" && not http.ua matches "(googlebot|facebook)" && port in {80 443}`)
-	if err != nil {
-		log.Printf("error %s", err)
+	schema := wirefilter.NewSchema()
+	defer schema.Close()
+
+	schema.AddFields(map[string]wirefilter.Type{
+		"http.request.method": wirefilter.TYPE_BYTES,
+		"http.user_agent":     wirefilter.TYPE_BYTES,
+		"ip.src":              wirefilter.TYPE_IP,
+		"ip.geoip.asnum":      wirefilter.TYPE_INT,
+	})
+
+	ctx := wirefilter.NewExecutionContext(schema)
+	defer ctx.Close()
+
+	ctx.SetFieldValue("http.request.method", "GET")
+	ctx.SetFieldValue("http.user_agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36")
+	//ctx.SetFieldValue("ip.src", net.ParseIP("1.1.1.1"))
+	ctx.SetFieldValue("ip.geoip.asnum", 13335)
+
+	rules := []string{
+		`http.request.method eq "GET"`,
+		`http.request.method eq "GET" and http.user_agent contains "Macintosh"`,
+		`http.request.method eq "GET" and http.user_agent not contains "Macintosh"`,
+		`http.request.method eq "GET" and not (http.user_agent contains "Macintosh")`,
+		`ip.geoip.asnum in {888 9910, 299}`,
+		`ip.geoip.asnum in {888 9910, 13335} and http.user_agent contains "Macintosh"`,
 	}
-	log.Printf("% #v", filter)
+
+	for _, rule := range rules {
+		ast, err := schema.Parse(rule)
+
+		if err != nil {
+			fmt.Print(rule)
+			fmt.Print(err)
+			continue
+		}
+
+		filter := ast.Compile()
+
+		fmt.Print(rule)
+		fmt.Print(" => ")
+		fmt.Print(filter.Execute(ctx))
+		fmt.Print("\n")
+	}
 }
--- wirefilter.go
+++ wirefilter.go
@@ -1,88 +1,200 @@
 package wirefilter
 
-// #cgo LDFLAGS: -L./lib -lwirefilter_ffi -Wl,-rpath=./lib
-// #cgo CFLAGS: -I./include
-// #cgo amd64 386 CFLAGS: -DX86=1
-// #include <stdbool.h>
-// #include "wirefilter.h"
-// typedef struct {
-//    uint8_t _res2;
-//    wirefilter_filter_ast_t *ast;
-// } wire_ast;
-// typedef struct {
-// 	uint8_t _res1;
+//#cgo LDFLAGS: -L${SRCDIR}/lib -Wl,-rpath,${SRCDIR}/lib -lwirefilter_ffi
+//#cgo CFLAGS: -I./include
+//#include <stdbool.h>
+//#include "wirefilter.h"
+//typedef struct {
+//	uint8_t _res2;
+//	wirefilter_filter_ast_t *ast;
+//} wirefilter_parsing_result_ok;
+//typedef struct {
+//	uint8_t _res1;
 // 	wirefilter_rust_allocated_str_t msg;
-// } wire_err;
-// typedef struct {
-//     const char *data;
-//     size_t length;
-// } rust_allocated_str;
+//} wirefilter_parsing_result_err;
 import "C"
 
 import (
 	"errors"
+	"log"
 	"net"
 	"reflect"
 	"unsafe"
 )
 
-type WirefilterScheme struct {
+type Type int
+
+const (
+	TYPE_IP    Type = C.WIREFILTER_TYPE_IP
+	TYPE_BYTES Type = C.WIREFILTER_TYPE_BYTES
+	TYPE_INT   Type = C.WIREFILTER_TYPE_INT
+	TYPE_BOOL  Type = C.WIREFILTER_TYPE_BOOL
+)
+
+type Schema struct {
 	ptr *C.wirefilter_scheme_t
+	types map[string]Type
 }
 
-func CreateScheme() *WirefilterScheme {
+func NewSchema() *Schema {
 	r := C.wirefilter_create_scheme()
-	return &WirefilterScheme{ptr: r}
+	//fmt.Printf("%T:%v\n", r, unsafe.Pointer(r))
+	return &Schema{ptr: r,
+		types: make(map[string]Type)}
 }
 
-func (scheme *WirefilterScheme) Close() error {
-	C.wirefilter_free_scheme(scheme.ptr)
-	return nil
+func (s *Schema) AddField(name string, type_ Type) {
+	cName := C.CString(name)
+	cNameSizeT := C.size_t(len(name))
+	defer C.free(unsafe.Pointer(cName))
+
+	C.wirefilter_add_type_field_to_scheme(s.ptr,
+		C.wirefilter_externally_allocated_str_t{
+			data: cName,
+			length: cNameSizeT,
+		}, C.wirefilter_type_t(type_)) // TODO: is this really safe?
+	s.types[name] = type_
 }
 
-type ParseFilter struct {
-	ptr unsafe.Pointer
-
-	Ast *Ast
+func (s *Schema) AddFields(fields map[string]Type) {
+	for field, type_ := range fields {
+		s.AddField(field, type_)
+	}
 }
 
-type Ast struct {
+func (s Schema) Parse(input string) (*AST, error) {
+	cInput := C.CString(input)
+	cInputSizeT := C.size_t(len(input))
+	defer C.free(unsafe.Pointer(cInput))
+
+	parsingResult := C.wirefilter_parse_filter(s.ptr,
+		C.wirefilter_externally_allocated_str_t{
+			data: cInput,
+			length: cInputSizeT,
+		})
+
+	parsingResultPtr := unsafe.Pointer(&parsingResult)
+
+	success := (*(*C.uint8_t)(parsingResultPtr))
+	if success > 0 {
+		_ok := (*C.wirefilter_parsing_result_ok)(parsingResultPtr)
+		return &AST{ptr: _ok.ast}, nil
+	} else {
+		_err := (*C.wirefilter_parsing_result_err)(parsingResultPtr)
+		s := C.wirefilter_rust_allocated_str_t(_err.msg)
+		return nil, errors.New(string(C.GoBytes(unsafe.Pointer(s.data), C.int(s.length))))
+	}
+}
+
+// TODO: wirefilter_free_parsing_result
+
+func (s *Schema) Close() {
+	C.wirefilter_free_scheme(s.ptr)
+}
+
+type AST struct {
 	ptr *C.wirefilter_filter_ast_t
 }
 
-func (scheme *WirefilterScheme) ParerFilter(exp string) (*ParseFilter, error) {
-	var result = &ParseFilter{}
-	r := C.wirefilter_parse_filter(scheme.ptr, C.wirefilter_externally_allocated_str_t(RString(exp)))
-	result.ptr = unsafe.Pointer(&r)
-
-	u := result.ptr
-	success := (*(*C.uint8_t)(u))
-	if success > 0 {
-		ast := (*C.wire_ast)(u)
-		result.Ast = &Ast{ptr: ast.ast}
-	} else {
-		_err := (*C.wire_err)(u)
-		return nil, errors.New(GString(C.rust_allocated_str(_err.msg)))
-	}
-
-	return result, nil
-}
-
-func (parse *ParseFilter) Close() error {
-	r := (*C.wirefilter_parsing_result_t)(parse.ptr)
-	C.wirefilter_free_parsing_result(*r)
-	return nil
-}
-
-func RString(s string) C.rust_allocated_str {
-	return C.rust_allocated_str{
-		length: C.size_t(len(s)),
-		data:   C.CString(s),
+func (ast *AST) Compile() *Filter {
+	compileResult := C.wirefilter_compile_filter(ast.ptr)
+	return &Filter{
+		ptr: compileResult,
 	}
 }
 
-func GString(s C.rust_allocated_str) string {
-	return string(C.GoBytes(unsafe.Pointer(s.data), C.int(s.length)))
+type Filter struct {
+	ptr *C.wirefilter_filter_t
+}
+
+func (f *Filter) Execute(ctx *ExecutionContext) bool {
+	r := C.wirefilter_match(f.ptr, ctx.ptr)
+	return bool(r)
+}
+
+type ExecutionContext struct {
+	ptr *C.wirefilter_execution_context_t
+	schema *Schema
+}
+
+func NewExecutionContext(schema *Schema) *ExecutionContext {
+	ctx := C.wirefilter_create_execution_context(schema.ptr)
+	return &ExecutionContext{
+		ptr: ctx,
+		schema: schema,
+	}
+}
+
+func (ctx *ExecutionContext) SetFieldValue(name string, value interface{}) {
+	_, ok := ctx.schema.types[name]
+
+	if !ok {
+		return
+	}
+
+	cName := C.CString(name)
+	cNameSizeT := C.size_t(len(name))
+	defer C.free(unsafe.Pointer(cName))
+
+	strTName := C.wirefilter_externally_allocated_str_t{
+		data: cName,
+		length: cNameSizeT,
+	}
+
+	switch value.(type) {
+	case int:
+		C.wirefilter_add_int_value_to_execution_context(
+			ctx.ptr, strTName, C.int(value.(int)))
+	case net.IP:
+		ip := value.(net.IP)
+		log.Print(ip)
+		/*
+		if value.(net.IP).To4() != nil {
+			var ipv4 [4]C.uint8_t
+			v := unsafe.Pointer(&ipv4)
+
+			sh := (*reflect.SliceHeader)(v)
+			sh.Data = uintptr(unsafe.Pointer(&ip[0]))
+			sh.Len  = len(ip)
+
+			C.wirefilter_add_ipv4_value_to_execution_context(
+				ctx.ptr, strTName, &ipv4)
+		} else {
+			var ipv6 [16]C.uint8_t
+
+			v := unsafe.Pointer(&ipv6)
+
+			sh := (*reflect.SliceHeader)(v)
+			sh.Data = uintptr(unsafe.Pointer(&ip[0]))
+			sh.Len  = len(ip)
+
+			C.wirefilter_add_ipv4_value_to_execution_context(
+				ctx.ptr, strTName, &ipv6)
+		}*/
+	case []byte:
+		buf := value.([]byte)
+		C.wirefilter_add_bytes_value_to_execution_context(
+			ctx.ptr, strTName, C.wirefilter_externally_allocated_byte_arr_t{
+				data: (*C.uchar)(unsafe.Pointer(&buf[0])), // TODO: free here needed?
+				length: C.size_t(len(buf)),
+			})
+	case string:
+		buf := []byte(value.(string))
+		ctx.SetFieldValue(name, buf)
+	case bool:
+		C.wirefilter_add_bool_value_to_execution_context(
+			ctx.ptr, strTName, C._Bool(value.(bool)))
+		break
+	}
+}
+
+func (ctx *ExecutionContext) Close() {
+	C.wirefilter_free_execution_context(ctx.ptr)
+}
+
+func Version() string {
+	versionResult := C.wirefilter_get_version()
+	return string(C.GoBytes(unsafe.Pointer(versionResult.data), C.int(versionResult.length)))
 }
 
 func IP2IP(ip net.IP) (ipv4 [4]C.uint8_t) {
